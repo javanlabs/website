f72fe06c7e6f07126f9d5f3596bd36e5b5c3f9c7
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2019-07-03 10:00:14 +0700

    styling footer

--- source/_layouts/footer.blade.php
+++ source/_layouts/footer.blade.php
@@ -1,56 +1,49 @@
-<footer class="bg-white py-8 w-full">
-    <div class="container mx-auto px-8">
-        <div class="table w-full">
-            <div class="block sm:table-cell">
-                <p class="uppercase text-grey text-sm sm:mb-6">Links</p>
-                <ul class="list-reset text-xs mb-6">
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">FAQ</a>
-                    </li>
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">Help</a>
-                    </li>
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">Support</a>
-                    </li>
-                </ul>
+<footer class="bg-blue-500 py-8 w-full text-white text-sm leading-loose p-5">
+    <div class="container mx-auto">
+        <!-- Five columns -->
+        <div class="flex flex-wrap -mx-5">
+            <div class="w-full lg:w-2/5 clearfix px-5 mb-5">
+                <div class="float-none md:float-left lg:float-left rounded-full bg-white w-32 h-32 mr-5 mb-5 flex items-center p-2 shadow">
+                    <img src="/assets/images/javan.png" class="w-32" alt="">
+                </div>
+                <div>
+                    <h3>PT Javan Cipta Solusi</h3>
+                    <p>Create integrated, innovative and creative IT solutions that meet market need since 2008.</p>
+                </div>
             </div>
-            <div class="block sm:table-cell">
-                <p class="uppercase text-grey text-sm sm:mb-6">Legal</p>
-                <ul class="list-reset text-xs mb-6">
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">Terms</a>
-                    </li>
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">Privacy</a>
-                    </li>
-                </ul>
+            <div class="w-full lg:w-1/5 px-5 mb-5">
+                <h3>Head Quarter</h3>
+                <address>
+                    Komplek Daichi No. 55, Jl. Terusan Jakarta Bandung
+                    <br>Jawa Barat, Indonesia.
+                    <br>+62-22-8783-1878
+                    <br>info@javan.co.id
+                </address>
             </div>
-            <div class="block sm:table-cell">
-                <p class="uppercase text-grey text-sm sm:mb-6">Social</p>
-                <ul class="list-reset text-xs mb-6">
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">Facebook</a>
-                    </li>
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">Linkedin</a>
-                    </li>
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">Twitter</a>
-                    </li>
-                </ul>
+            <div class="w-full lg:w-1/5 px-5 mb-5">
+                <h3>Research & Development</h3>
+                <address>
+                    Perumahan Pamungkas B-08
+                    <br>Sleman, Yogyakarta
+                </address>
             </div>
-            <div class="block sm:table-cell">
-                <p class="uppercase text-grey text-sm sm:mb-6">Company</p>
-                <ul class="list-reset text-xs mb-6">
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">Official Blog</a>
+            <div class="w-full lg:w-1/5 px-5 mb-5">
+                <h3>Links</h3>
+                <ul class="list-reset mb-6">
+                    <li class="inline-block mr-2 sm:block sm:mr-0">
+                        <a href="#" class="text-grey hover:text-grey-dark">Products</a>
                     </li>
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">About Us</a>
+                    <li class="inline-block mr-2 sm:block sm:mr-0">
+                        <a href="#" class="text-grey hover:text-grey-dark">Our Services</a>
                     </li>
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">Contact</a>
+                    <li class="inline-block mr-2 sm:block sm:mr-0">
+                        <a href="#" class="text-grey hover:text-grey-dark">Company</a>
+                    </li>
+                    <li class="inline-block mr-2 sm:block sm:mr-0">
+                        <a href="#" class="text-grey hover:text-grey-dark">Contact Us</a>
+                    </li>
+                    <li class="inline-block mr-2 sm:block sm:mr-0">
+                        <a href="#" class="text-grey hover:text-grey-dark">Blog</a>
                     </li>
                 </ul>
             </div>
