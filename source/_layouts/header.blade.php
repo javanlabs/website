b3a7c28d450848457da15015dfdb5d079e6029ec
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2019-07-04 12:50:36 +0700

    Dropdown menu

--- source/_layouts/header.blade.php
+++ source/_layouts/header.blade.php
@@ -1,17 +1,36 @@
 <header class="bg-white shadow">
     <div class="container mx-auto px-4">
-        <div class="flex items-center justify-between py-2">
+        <div class="flex items-center justify-between">
             <div>
                 <img src="/assets/images/javan.png" class="h-10 lg:h-20" alt="Logo PT Javan Cipta Solusi">
             </div>
 
-            <div class="flex items-end">
-                <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mx-2 md:mx-4 lg:mx-6">Products</a>
-                <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mx-2 md:mx-4 lg:mx-6">Our Services</a>
-                <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mx-2 md:mx-4 lg:mx-6">Company</a>
-                <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mx-2 md:mx-4 lg:mx-6">Contact Us</a>
-                <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mx-2 md:mx-4 lg:mx-6">Blog</a>
-            </div>
+            <ul class="flex items-end dropdown">
+                <li class="text-gray-800 text-sm font-semibold mx-2 md:mx-4 lg:mx-6">
+                    <a href="#">Products</a>
+                </li>
+                <li class="text-gray-800 text-sm font-semibold mx-2 md:mx-4 lg:mx-6">
+                    <a href="#">Our Services</a>
+                    <ul>
+                        <li><a href="#">Business Optimalization</a></li>
+                        <li><a href="#">Web/Mobile App Development</a></li>
+                        <li><a href="#">Data Analaytics</a></li>
+                        <li><a href="#">Production Development</a></li>
+                    </ul>
+                </li>
+                <li class="text-gray-800 text-sm font-semibold mx-2 md:mx-4 lg:mx-6">
+                    <a href="#">Company</a>
+                    <ul>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Team</a></li>
+                        <li><a href="#">Career</a></li>
+                    </ul>
+                </li>
+                <li class="text-gray-800 text-sm font-semibold mx-2 md:mx-4 lg:mx-6"><a
+                            href="#">Contact Us</a></li>
+                <li class="text-gray-800 text-sm font-semibold mx-2 md:mx-4 lg:mx-6"><a
+                            href="#">Blog</a></li>
+            </ul>
         </div>
     </div>
 </header>
